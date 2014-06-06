afb05d68ef912d370e6a93cda66f5283b255934f
Scott Douglass <scott@scottyd.net>
2014-06-05 21:58:07 -0400

    started chapter 3

---
+++ ch3/blog/js/app/app.js
@@ -0,0 +1,23 @@
+var Blog = Ember.Application.create();
+
+Blog.Router = Ember.Router.extend({
+	location: 'hash'
+});
+
+Blog.Router.map(function() {
+	this.route("index", {path: "/"});
+	this.route("blog", {path: "blog"});
+});
+
+Blog.IndexRoute = Ember.Route.extend({
+	redirect: function() {
+		this.transitionTo('blog');
+	}
+});
+
+Blog.BlogRoute = Ember.Route.extend({
+	model: function() {
+		return this.store.find('blogPost');
+	}
+});
+
